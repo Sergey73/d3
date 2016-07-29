25d7dd8bd7faa2046fb1b082e53b4b0787306744
Sergey Hripchenko <sergey.khripchenko@simbirsoft.com>
2016-07-29 18:49:56 +0400

    add barChart

--- js/people-count.js
+++ js/people-count.js
@@ -3,102 +3,89 @@
 	var metropolis = {};
 	var shuka = {};
 
+  // загружаем данные
   d3.csv("./data/visitors.csv", function(error, data) {
     if (error) throw error;
+
+    // тк значения данных приходят в строковом виде, преобразуем соответствующие 
+    // значения полей в числа и даду  
     preParceDann("dt","%Y-%m-%d-%H",["age", "customers_cnt", "gender", "hour"], data);
 
     // разбиваем данные на категории по торговым центрам
     var dataGroup = d3.nest()
           .key(function(d) { return d.tid; })
-          .entries(data);
-    console.dir(dataGroup);
+          .entries(data);      
     dataGroup.forEach(function (shop) {
     	shop.key == "Himki" ? himki.values = shop.values :
     		shop.key == "Shuka" ? shuka.values = shop.values :
-	    		shop.key == "Metropolis" ? metropolis.values = shop.values : null;
+	    		shop.key == "Metropolis" ? metropolis.values = shop.values : null
     });
 
     // минимальное и максимальное значение даты торговых центров
     himki['minMaxDate'] = getMinMaxDate(himki.values);
-    // metropolis['minMaxDate'] = getMinMaxDate(shuka.values);
-    // shuka['minMaxDate'] = getMinMaxDate(metropolis.values);
+    metropolis['minMaxDate'] = getMinMaxDate(shuka.values);
+    shuka['minMaxDate'] = getMinMaxDate(metropolis.values);
 
-    var himkiDate = crossfilter(himki.values);
-    // var all = himkiDate.groupAll();
+    // преобразование данных с попошью библиотеки crossfilter
+    var dataCross = crossfilter(himki.values);
+    // dimension по дате
+    var dateByDay = dataCross.dimension(function(d) { return d.dt; });
+    var all = dataCross.groupAll();
 
-    // упорядочиваем значения от большего к меньшему по полю dt
-    var date = himkiDate.dimension(function(d) { return d.dt; });
-    var dates = date.group(d3.time.day);
-    // dates.groupId = "dates";
+    // создаем группы по дням
+    var daysGroup = dateByDay.group(d3.time.day)
+    // считаем количество людей посетивших торговый центр за день
+      .reduce(reduceAdd, reduceRemove, reduceInitial);
+     
+    // создаем группы по часам
+    var hoursGroup = dateByDay.group(d3.time.hour)
+    // считаем количество людей посетивших торговый центр за час
+      .reduce(reduceAdd, reduceRemove, reduceInitial);
 
-    var people = himkiDate.dimension(function(d) { return d.customers_cnt; });
-    var peoples = people.group(Math.floor);
+    function reduceAdd(p, v) {
+      p += v.customers_cnt;
+      return p;
+    }
 
-    // упорядочиваем значения от большего к меньшему по полю dt
-    var hour = himkiDate.dimension(function(d) { return d.hour; });
-    var hours = hour.group(Math.floor);
-    // hours.groupId = "hours";
+    function reduceRemove(p, v) {
+      p -= v.customers_cnt;
+      return p;
+    }
+
+    function reduceInitial() {
+      return 0;
+    }
+
+    // максимальное посещение в день
+    var maxPeopleInDay = d3.max(daysGroup.top(Infinity), function (d) { return d.value; });
+    // максимальное посещение за час
+    var maxPeopleInHour = d3.max(hoursGroup.top(Infinity), function (d) { return d.value; });
+  
+    // создаем график 
     var chart = dc.barChart("#peopleCount");
+    
     chart
-      .width(800)
-      .height(800)
-      .x(d3.time.scale().domain([new Date(himki.minMaxDate[0]), new Date(himki.minMaxDate[1])]))
-      .xUnits(d3.time.month)
-      // .gap(5)
-      .dimension(peoples)
-      .group(dates)
-     // .x(d3.scale.linear().domain(himki.minMaxDate))
-      // .x(d3.time.scale().domain(himki.minMaxDate))
-    //    .round(d3.time.month.round)
-    // .elasticY(true);
-      .brushOn(true)
-      // .yAxisLabel("This is the Y Axis!")
-      .on('renderlet', function(chart) {
-          chart.selectAll('rect').on("click", function(d) {
-              console.log("click!", d);
-          });
-      });
-      chart.render();
-
-    // console.dir(date);
-
-    // var chart = dc.barChart("#peopleCount");
-    // d3.csv("./data/morley.csv", function(error, experiments) {
-
-    //   experiments.forEach(function(x) {
-    //     x.Speed = +x.Speed;
-    //   });
-
-    //   var ndx                 = crossfilter(experiments),
-    //       runDimension        = ndx.dimension(function(d) {return +d.Run;}),
-    //       speedSumGroup       = runDimension.group().reduceSum(function(d) {return d.Speed * d.Run / 1000;});
-
-    //   chart
-    //     .width(768)
-    //     .height(480)
-    //     .x(d3.scale.linear().domain([6,20]))
-    //     .brushOn(false)
-    //     .yAxisLabel("This is the Y Axis!")
-    //     .dimension(runDimension)
-    //     .group(speedSumGroup)
-    //     .on('renderlet', function(chart) {
-    //         chart.selectAll('rect').on("click", function(d) {
-    //             console.log("click!", d);
-    //         });
-    //     });
-    //     chart.render();
-    // });
-
-
-
-
-
-
+      .width(1000)
+      .height(200)
+      .x(d3.time.scale([new Date(himki.minMaxDate[0]), new Date(himki.minMaxDate[1])]))
+      .y(d3.scale.linear().domain([0, maxPeopleInDay]))
+      .xUnits(d3.time.days)
+      .elasticX(true)
+      .xAxisPadding(1)
+      .yAxisLabel("Количество людей")
+      .dimension(dateByDay)
+      .group(daysGroup)
+      .margins({top: 20, left: 80, right: 10, bottom: 30})
+      .transitionDuration(1500)
+      .colors('red')
+      .centerBar(true)
+      .barPadding(1)
+      .render();
   });
 
-  function getMinMaxDate (data) {
+  function getMinMaxDate(data) {
     return d3.extent(data, function(d) { 
-    	return d.dt; 
+      return d.dt; 
     });
   }
 
